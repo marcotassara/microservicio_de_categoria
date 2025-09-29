36531d3892301b27c4e88dc5f0adb582f113f502
Marco Tassara :v <ma.tassara@duocuc.cl>
2025-09-29 14:22:14 -0300

    Se arregla ruta debido a interferencia con coneccion con microservicio producto (/) ahora se conectan perfectamente, se agrega ademas un try catch para que tenga soporte de errores

--- src/main/java/com/proyecto/categoria/model/Categoria.java
+++ src/main/java/com/proyecto/categoria/model/Categoria.java
@@ -10,6 +10,7 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
     @SequenceGenerator(name = "categoria_seq", sequenceName = "CATEGORIA_SEQ", allocationSize = 1)
+    @Column(name = "IDCATEGORIA", length = 50)
     private Long id;
     
     @Column(name = "NOMBRE", nullable = false, length = 100, unique = true)
